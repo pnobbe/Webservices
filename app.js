9643b319321298feedfa2eaf9115866f9f79b06a
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-02-28 17:46:41 +0100

    Added Swagger - start project with "swagger project start"

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var SwaggerExpress = require('swagger-express-mw');
 
 // Data Access Layer
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/bookstore');
 mongoose.Promise = require('q').Promise;
 //mongoose.Promise = global.Promise;
+
 // /Data Access Layer
 
 // Models
@@ -17,6 +21,10 @@ require('./models/book');
 require('./models/author');
 require('./models/fillTestData')();
 // /Models
+
+var config = {
+    appRoot: __dirname // required config
+};
 
 function handleError(req, res, statusCode, message){
     console.log();
@@ -31,51 +39,31 @@ function handleError(req, res, statusCode, message){
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var config = {
+    appRoot: __dirname // required config
+};
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+    if (err) { throw err; }
+
+    // install middleware
+    swaggerExpress.register(app);
+
+    // Serve the Swagger documents and Swagger UI
+    app.use(swaggerExpress.runner.swaggerTools.swaggerUi());
+
+    var port = process.env.PORT || 10010;
+    app.listen(port);
+
+    if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
-app.use('/books', require('./routes/books')(handleError));
-app.use('/authors', require('./routes/authors')(handleError));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
+module.exports = app; // for testing
