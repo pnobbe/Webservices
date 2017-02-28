258d60b5fc97101bc0a15d46fe7bf855ab286da9
Marius de Vogel <mvogel@student.avans.nl>
2017-02-28 18:11:33 +0100

    added gitignore, models and very simple testdata

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ var bodyParser = require('body-parser');
 
 // Data Access Layer
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bookstore');
-mongoose.Promise = require('q').Promise;
-//mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/kroegentocht');
+//mongoose.Promise = require('q').Promise;
+mongoose.Promise = global.Promise;
 // /Data Access Layer
 
 // Models
-require('./models/book');
-require('./models/author');
+require('./models/user');
+require('./models/waypoint');
+require('./models/race');
 require('./models/fillTestData')();
 // /Models
 
-function handleError(req, res, statusCode, message){
+function handleError(req, res, statusCode, message) {
     console.log();
     console.log('-------- Error handled --------');
     console.log('Request Params: ' + JSON.stringify(req.params));
@@ -39,16 +40,16 @@ app.set('view engine', 'pug');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/books', require('./routes/books')(handleError));
-app.use('/authors', require('./routes/authors')(handleError));
+//app.use('/books', require('./routes/books')(handleError));
+//app.use('/authors', require('./routes/authors')(handleError));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -59,7 +60,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -70,7 +71,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
