5feb8af5df447e67bfd4a1014db02d0a57ec916b
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-04-05 00:50:14 +0200

    d

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,5 @@
 //const socket = io.connect('http://127.0.0.1:8080', {secure: true});
-const socket = io.connect();
+const socket = io.connect("https://restracer.herokuapp.com", {secure: true});
 
 socket.on('connect', function () {
     console.log("Successfully connected to socket on port ");
