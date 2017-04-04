91097ee687b5c2f3861785cc47bfd68819d99a5e
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-04-05 00:29:17 +0200

    esad

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,5 @@
 //const socket = io.connect('http://127.0.0.1:3001', {secure: true});
-const socket = io.connect("176.34.228.27:3001", {secure: true});
+const socket = io.connect("http://restracer.herokuapp.com:3001", {secure: true});
 
 socket.on('connect', function () {
     console.log("Successfully connected to socket on port ");
