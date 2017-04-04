2fa5fa8963d04b23b39b3baf40e2f5956d143a63
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-04-04 23:35:35 +0200

    test

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,5 @@
-const socket = io.connect('http://127.0.0.1:3001', {secure: true});
-//const socket = io.connect("https://restrace.herokuapp.com",{secure: true});
+//const socket = io.connect('http://127.0.0.1:3001', {secure: true});
+const socket = io.connect("https://restrace.herokuapp.com",{secure: true});
 
 socket.on('connect', function () {
     console.log("Successfully connected to socket on port ");
